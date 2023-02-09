8ee785413a691daf2eb0a19062171a20fa9832fc
Nelsen Niko <123928431+nelsenpro@users.noreply.github.com>
2023-02-09 10:07:43 +0700

    Add files via upload

---
+++ framecss/modif.js
@@ -0,0 +1,21 @@
+// modifikasi JavaScript disini
+// 
+console.log('instalasi berhasil');
+// mulai modifikasi....
+// *****************************************
+function nav2Function() {
+    var x = document.getElementById("nav2");
+    if (x.className === "nav2") {
+        x.className += " responsive2";
+    } else {
+        x.className = "nav2";
+    }
+};
+
+/*
+modifikasi JavaScript sesuai keinginan mu
+*animasi
+*durasi
+*filter
+*efek
+*/
